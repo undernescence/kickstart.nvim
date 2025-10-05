f4f4b45123fb97fc2157f874a67020adf8e165a7
undernescence <kreppold98@gmail.com>
2025-10-05 12:53:42 +0200

    Add keymap to open the explorer

--- init.lua
+++ init.lua
@@ -205,6 +205,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Open Explorer
+vim.keymap.set('n', '<leader>ex', ':Ex<CR>', { desc = 'Open the explorer' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
